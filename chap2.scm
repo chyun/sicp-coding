993fffb7bfac2add49c0b55ea9c11f8c0b8e2ae9
liqun.wu <liqun.wu@dianping.com>
2016-01-17 02:02:54 +0800

    update

--- chap2.scm
+++ chap2.scm
@@ -145,3 +145,242 @@
 						(enumerate-interval 1 (- j 1))))
 				(enumerate-interval 1 (- i 1))))
 		(enumerate-interval 1 n)))
+
+(define (adjoin-position new-row k rest-of-queens)
+    (cons new-row rest-of-queens))
+
+(define (queens board-size)
+  (define (queen-cols k)  
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+(define empty-board '())
+
+(define (iter-check row-of-new-queen rest-of-queens i)
+    (if (null? rest-of-queens)  ; 下方所有皇后检查完毕, 新皇后安全
+        #t
+        (let ((row-of-current-queen (car rest-of-queens)))
+            (if (or (= row-of-new-queen row-of-current-queen)           ; 行碰撞
+                    (= row-of-new-queen (+ i row-of-current-queen))     ; 右下方碰撞
+                    (= row-of-new-queen (- row-of-current-queen i)))    ; 左下方碰撞
+                #f
+                (iter-check row-of-new-queen 
+                            (cdr rest-of-queens)    ; 继续检查剩余的皇后
+                            (+ i 1))))))            ; 更新步进值
+
+(define (square-of-four tl tr bl br)
+  (lambda (painter)
+    (let ((top (beside (tl painter) (tr painter)))
+          (bottom (beside (bl painter) (br painter))))
+      (below bottom top))))
+
+(define (flipped-pairs painter)
+  (let ((combine4 (square-of-four identity flip-vert
+                                  identity flip-vert)))
+    (combine4 painter)))
+
+(define (square-limit painter n)
+  (let ((combine4 (square-of-four flip-horiz identity
+                                  rotate180 flip-vert)))
+    (combine4 (corner-split painter n))))
+
+(define (make-vect xcor ycor)
+    (list xcor ycor))
+
+(define (xcor-vect v)
+    (car v))
+
+(define (ycor-vect v)
+    (cadr v))
+
+(define (add-vect vect another-vect)
+  (cond ((null? vect) another-vect)
+        ((null? another-vect) vect)
+        ((make-vect (+ (xcor-vect vect)
+                        (xcor-vect another-vect))
+                     (+ (ycor-vect vect)
+                        (ycor-vect another-vect))))))
+
+(define (sub-vect vect another-vect)
+  (cond ((null? vect) '())
+        ((null? another-vect) '())
+        ((make-vect (- (xcor-vect vect)
+                       (xcor-vect another-vect))
+                    (- (ycor-vect vect)
+                       (ycor-vect another-vect))))))
+
+(define (scale-vect factor vect)
+    (make-vect (* factor (xcor-vect vect))
+               (* factor (ycor-vect vect))))
+
+(define (make-frame origin edge1 edge2)
+    (list origin edge1 edge2))
+
+(define (origin-frame f)
+    (car f))
+
+(define (edge1-frame f)
+    (cadr f))
+
+(define (edge2-frame f)
+    (caddr f))
+
+(define (make-frame origin edge1 edge2)
+    (cons origin
+          (cons edge1 edge2)))
+
+(define (origin-frame f)
+    (car f))
+
+(define (edge1-frame f)
+    (cadr f))
+
+(define (edge2-frame f)
+    (cddr f))
+
+(define (frame-coord-map frame)
+    (lambda (v)
+        (add-vect
+            (origin-frame frame)
+            (add-vect 
+                (scale-vect (xcor-vect v)
+                            (edge1-frame frame))
+                (scale-vect (ycor-vect v)
+                           (edge2-frame frame))))))
+
+(define (segments->painter segment-list)
+    (lambda (frame)
+        (for-each
+            (lambda (segment)
+                (draw-line
+                    ((frame-coord-map frame) (start-segment segment))
+                    ((frame-coord-map frame) (end-segment segment))))
+        segment-list)))
+
+(define (make-segment start end)
+    (list start end))
+
+(define (start-segment s)
+    (car s))
+
+(define (end-segment s)
+    (cadr s))
+
+(define top-left (make-vect 0.0 1.0))
+
+(define top-right (make-vect 1.0 1.0))
+
+(define bottom-left (make-vect 0.0 0.0))
+
+(define bottom-right (make-vect 1.0 0.0))
+
+(define top (make-segment top-left top-right))
+
+(define left (make-segment top-left bottom-left))
+
+(define right (make-segment top-right bottom-right))
+
+(define bottom (make-segment bottom-left bottom-right))
+
+(define (transform-painter painter origin corner1 corner2)
+  (lambda (frame)
+    (let ((m (frame-coord-map frame)))
+      (let ((new-origin (m origin)))
+        (painter
+         (make-frame new-origin
+                     (sub-vect (m corner1) new-origin)
+                     (sub-vect (m corner2) new-origin)))))))
+
+(define (flip-vert painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)   ; new origin
+                     (make-vect 1.0 1.0)   ; new end of edge1
+                     (make-vect 0.0 0.0))) ; new end of edge2
+
+(define (shrink-to-upper-right painter)
+  (transform-painter painter
+                     (make-vect 0.5 0.5)
+                     (make-vect 1.0 0.5)
+                     (make-vect 0.5 1.0)))
+
+(define (rotate90 painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+(define (squash-inwards painter)
+  (transform-painter painter
+                     (make-vect 0.0 0.0)
+                     (make-vect 0.65 0.35)
+                     (make-vect 0.35 0.65)))
+
+(define (beside painter1 painter2)
+  (let ((split-point (make-vect 0.5 0.0)))
+    (let ((paint-left
+           (transform-painter painter1
+                              (make-vect 0.0 0.0)
+                              split-point
+                              (make-vect 0.0 1.0)))
+          (paint-right
+           (transform-painter painter2
+                              split-point
+                              (make-vect 1.0 0.0)
+                              (make-vect 0.5 1.0))))
+      (lambda (frame)
+        (paint-left frame)
+        (paint-right frame)))))
+
+(define (flip-horiz painter)
+    (transform-painter painter
+                       (make-vect 1.0 0.0)
+                       (make-vect 0.0 0.0)
+                       (make-vect 1.0 1.0)))
+
+(define (below painter1 painter2)
+    (lambda (frame)
+        ((flip-horiz
+            (rotate90
+                (beside
+                    (rotate270
+                        (flip-horiz painter1))
+                    (rotate270
+                        (flip-horiz painter2)))))
+         frame)))
+
+(define (memq item x)
+  (cond ((null? x) false)
+        ((eq? item (car x)) x)
+        (else (memq item (cdr x)))))
+
+(memq 'apple '(pear banana prune))
+
+(define (equal? x y)
+    (cond ((and (symbol? x) (symbol? y))
+            (symbol-equal? x y))
+          ((and (list? x) (list? y))
+            (list-equal? x y))
+          (else
+            (error "Wrong type input x and y -- EQUAL?" x y))))
+
+(define (symbol-equal? x y)
+    (eq? x y))
+
+(define (list-equal? x y)
+    (cond ((and (null? x) (null? y))    ; 空表
+            #t)
+          ((or (null? x) (null? y))     ; 长度不同的表
+            #f)
+          ((equal? (car x) (car y))     ; 对比 car 部分
+            (equal? (cdr x) (cdr y)))   ; 递归对比 cdr 部分
+          (else
+            #f)))
